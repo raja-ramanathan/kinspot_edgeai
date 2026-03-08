88b74b4d7b2d1ec1bacf3b63e6863b24a4bd73ba
arogya-parinam <arogya.parinam@gmail.com>
2026-03-08 01:20:25 -0800

    refactor to use face_recoginition module

--- kinspot_embeddings.py
+++ kinspot_embeddings.py
@@ -1,77 +1,51 @@
 import cv2
 import torch
-import time
-from torch.nn.functional import cosine_similarity
+from face_recognition import FaceDetector, FaceRecognitionSystem
 from main import KinspotModelLoader, DEVICE
-from torchvision import transforms
 import torch.nn.functional as F
+from torch.nn.functional import cosine_similarity
 
-cap = cv2.VideoCapture(0, cv2.CAP_AVFOUNDATION)
-time.sleep(5)
+class FacePredictor:
+    def __init__(self):
+        self.confidence_threshold = 0.85  # adjust as needed
+        self.modelLoader = KinspotModelLoader()
+        self.id2label = self.modelLoader.id2label
+        self.model = self.modelLoader.model
+        self.transform = self.modelLoader.transform
+        self.label_db_file = "embeddings/kinspot_embeddings.pt"
+        self.label_db = torch.load(self.label_db_file)
 
-confidence_threshold = 0.85  # adjust as needed
+    def _extract_embedding(self, face_img):
+        input_tensor = self.transform(face_img).unsqueeze(0).to(DEVICE)
+        with torch.no_grad():
+            outputs = self.model.vit(pixel_values=input_tensor)
+            embedding = outputs.last_hidden_state[:, 0, :]  # CLS token
+            embedding = F.normalize(embedding, p=2, dim=1)
+        return embedding
 
-modelLoader = KinspotModelLoader()
-model = modelLoader.model
-processor = modelLoader.processor
-transform = modelLoader.transform
-id2label = modelLoader.id2label
-label_db_file = "embeddings/kinspot_embeddings.pt"
-label_db = torch.load(label_db_file)
+    def _find_best_match(self,embedding):
+        best_score = -1
+        best_name = None
+        for name, db_emb in self.label_db.items():
+            score = cosine_similarity(embedding, db_emb.unsqueeze(0))
+            if score > best_score:
+                best_score = score
+                best_name = name
+        return best_name, best_score.item()
 
-def extract_embedding(face_img):
-    input_tensor = transform(face_img).unsqueeze(0).to(DEVICE)
-    with torch.no_grad():
-        outputs = model.vit(pixel_values=input_tensor)
-        embedding = outputs.last_hidden_state[:, 0, :]   # CLS token
-        embedding = F.normalize(embedding, p=2, dim=1)
-    return embedding
-
-def find_best_match(embedding):
-
-    best_score = -1
-    best_name = None
-
-    for name, db_emb in label_db.items():
-        score = cosine_similarity(embedding, db_emb.unsqueeze(0))
-        if score > best_score:
-            best_score = score
-            best_name = name
-    return best_name, best_score.item()
+    def predict_face(self, face_img):
+            embedding = self._extract_embedding(face_img)
+            best_name, best_score = self._find_best_match(embedding)
+            if best_score > self.confidence_threshold:
+                return best_name, best_score
+            else:
+                return "Not a family member", best_score
 
 def main():
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-    print("Starting....")
-    label = "UNKNOWN"
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-        for (x, y, w, h) in faces:
-            face = frame[y:y+h, x:x+w]
-            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-            embedding = extract_embedding(face)
-            label, confidence = find_best_match(embedding)
-            print(label,confidence)
-            if not (confidence > confidence_threshold):
-                label = "Not a family member"
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 2)
-            cv2.putText(frame,
-                        f"{label} ({confidence:.2f})",
-                        (x, y-10),
-                        cv2.FONT_HERSHEY_SIMPLEX,
-                        0.8,
-                        (0,255,0),
-                        2)
-        cv2.imshow("Family Recognition", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            print("exiting....")
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
+    face_predictor = FacePredictor()
+    face_recognition_system = FaceRecognitionSystem(face_predictor)
+    face_recognition_system.start()
+    face_recognition_system.shutdown()
 
 if __name__ == "__main__":
     main()
